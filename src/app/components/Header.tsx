f7177c3b493990d7d3052207771c5021665d1690
Anton <94073149+Harynets@users.noreply.github.com>
2025-09-10 13:29:40 +0300

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,9 +5,9 @@ function Header() {
     return (
         <div className="p-1 flex justify-end items-center bg-[#f8f9fa]">
             <div className="flex social-media mx-4">
-                <img className="h-10 mx-1" src="/facebook.svg" alt="facebook"></img>
-                <img className="h-10 mx-1" src="/instagram.svg" alt="instagram"></img>
-                <img className="h-10 mx-1" src="/youtube.svg" alt="youtube"></img>
+                <img className="h-10 mx-1" src="/WebLab/facebook.svg" alt="facebook"></img>
+                <img className="h-10 mx-1" src="/WebLab/instagram.svg" alt="instagram"></img>
+                <img className="h-10 mx-1" src="/WebLab/youtube.svg" alt="youtube"></img>
             </div>
             <Link
                 className="cursor-pointer hover:bg-[#60deb4] active:scale-95 bg-[#3ed9a5] border-1 border-gray-300 rounded-md px-2 py-1.5"
