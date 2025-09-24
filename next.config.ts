a3dad4989d6695b4ec8f705599b4324285a8498d
Anton <garynets.anton@gmail.com>
2025-09-24 18:16:05 +0300

    Add lab3

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/WebLab",
-  assetPrefix: "/WebLab/",
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+import TouristPlace from "./components/TouristPlace";
+import TouristPlaceList from "./components/TouristPlaceList";
+import { useState } from "react";
+import UpdateForm from "./components/UpdateForm";
+
 export default function Home() {
-    return <></>;
+    const touristPlaces = [
+        {
+            placeName: "Колізей",
+            countryName: "Італія",
+            description: "Одне з 7 Нових чудес світу, античний амфітеатр, велич у “Вічному місті”.",
+        },
+        { placeName: "Ейфелева Вежа", countryName: "Франція", description: "Унікальна споруда, символ міста закоханих." },
+    ];
+
+    const [touristPlace, setTouristPlace] = useState({
+        placeName: "test",
+        countryName: "test",
+        description: "test",
+    });
+
+    return (
+        <div>
+            <div className="flex justify-center">
+                <TouristPlaceList touristPlaces={touristPlaces} />
+            </div>
+            <div className="flex justify-center">
+                <TouristPlace
+                    placeName={touristPlace.placeName}
+                    description={touristPlace.description}
+                    countryName={touristPlace.countryName}
+                />
+            </div>
+            <div className="flex justify-center">
+                <UpdateForm setTouristPlace={setTouristPlace}></UpdateForm>
+            </div>
+        </div>
+    );
 }
